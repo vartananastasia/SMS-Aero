f279e0674c4071e0919fb954a7aca877470c087a
nastia <vartan.anastasia@ya.ru>
2017-07-10 15:19:58 +0300

    note changed

--- SMS/Client.php
+++ SMS/Client.php
@@ -15,7 +15,7 @@ class Client
     private $password;
 
     # subscription in sms
-    const from = 'NEWS';  // Change subscription
+    const from = 'LEXAND';  // Change subscription
 
     # chanel types
     const TYPE_4 = 4;
